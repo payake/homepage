b4fb2c62da9b193638b7680dd6fdef09a4fcedf2
Peter Yake <peter.yake@gmail.com>
2024-12-10 21:39:10 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://yourusername.github.io',
+  base: '/your-repo-name',
 });
